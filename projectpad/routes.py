8c6a0ffc48265c37644f6ce1f8a928120d66d65e
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-03-30 14:21:36 +0300

    account profile update feature

--- projectpad/routes.py
+++ projectpad/routes.py
@@ -1,6 +1,9 @@
+import os
+import secrets
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from projectpad import app, db
-from projectpad.forms import RegistrationForm, LoginForm
+from projectpad.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from projectpad.models import User, Article
 from flask_login import login_user, current_user, logout_user, login_required
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -76,9 +79,38 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
+def save_picture(form_picture):
+    """ Saves and returns the picture uploaded via form """
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/images', picture_fn)
 
-@app.route("/account")
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
+
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
     """Returns the account page"""
-    return render_template('account.html', title='Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='images/' + current_user.image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
+
